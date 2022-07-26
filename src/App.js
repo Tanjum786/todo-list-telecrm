54fc4107e85a802818fd0bd4d82e9208c5dd5bff
tanjum786 <tanjunkadakol147@gmail.com>
2022-07-26 12:38:22 +0530

    feat: render todo component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { Navbar } from "./Component/Navbar";
+import { TodoInput } from "./Component/Todos/TodoInput";
+import { Todos } from "./Component/Todos/Todos";
+import { useTodo } from "./context/TodoContext";
 
 function App() {
+  const [todoInput, setTodoinput] = useState("");
+  const [editedTodo, setEditedTodo] = useState(null);
+
+  const { todoList } = useTodo();
+  const { TodoList } = todoList;
   return (
     <div>
+      <Navbar />
+      <TodoInput
+        todoInput={todoInput}
+        setTodoinput={setTodoinput}
+        editedTodo={editedTodo}
+        setEditedTodo={setEditedTodo}
+      />
+      <div className="allTodosContainer">
+        <h1 className="title">All Todos ({TodoList?.length})</h1>
+        {TodoList?.map((todos) => {
+          return (
+            <Todos
+            key={todos.id}
+              setTodoinput={setTodoinput}
+              setEditedTodo={setEditedTodo}
+              todos={todos}
+            />
+          );
+        })}
       </div>
+    </div>
   );
 }
 
